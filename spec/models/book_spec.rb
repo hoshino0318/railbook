6d076b08f544f4c4cfb8b45ea022d7499fc16167
HOSHINO Tatsuya <tatsuya.hoshino@nts.ricoh.co.jp>
2011-11-09 17:43:05 +0900

    add test case for Book.isbn field

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,17 +1,31 @@
 require 'spec_helper'
 
 describe Book do
-  describe "size isbn of Book must be 17." do
+  describe "isbn length must be 17." do
     describe "#save" do
       [
        ["1"*17, true],
        ["1"*18, false],
       ].each do |isbn, expected|
-        it "returns #{expected} if isbn size is #{isbn.size} (#{isbn.to_s})" do
+        it "returns #{expected} if isbn length is #{isbn.size} (#{isbn.to_s})" do
           book = Book.new({:isbn => isbn})
           book.save.should == expected
         end
       end
     end
   end
+  
+  describe "isbn value must be presence." do
+    describe "#save" do
+      it "returns true when isbn is 11111111111111111. " do
+        book = Book.new({:isbn => "1"*17})
+        book.save.should == true
+      end
+      it "returns false when isbn is empty "do
+        book = Book.new()
+        book.save.should == false
+      end
+    end
+  end
 end
+
